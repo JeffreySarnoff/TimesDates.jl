3e29bec3d70bfe3f12587b5e92ada435c4f4eb65
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-03 12:34:04 -0500

    Create DatingTime.jl

---
+++ src/DatingTime.jl
@@ -0,0 +1,13 @@
+module DatingTime
+
+export TimeDate, ZonedTimeDate
+
+import TimeZones: ZonedDateTime
+
+include("TimeDate.jl")
+include("ZonedTimeDate.jl")
+
+include("transitive_conversion.jl")
+include("ringoid_promotion.jl")
+
+end  # DatingTime
