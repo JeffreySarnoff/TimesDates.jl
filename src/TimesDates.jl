707d1132b6e6df9cabb9280f571cc5933f69c610
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-09 23:03:40 -0500

    Update TimesDates.jl

--- src/TimesDates.jl
+++ src/TimesDates.jl
@@ -435,11 +435,11 @@ function Base.:(!=)(atd::TimeDate, btd::TimeDate)
 end
 function Base.:(<)(atd::TimeDate, btd::TimeDate)
     delta = atd - btd
-    return !isempty(delta) && signbit(delta.periods[1])
+    return !isempty(delta) && signbit(delta.periods[1].value)
 end    
 function Base.:(<=)(atd::TimeDate, btd::TimeDate)
     delta = atd - btd
-    return isempty(delta) || signbit(delta.periods[1])
+    return isempty(delta) || signbit(delta.periods[1].value)
 end    
 Base.isequal(atd::TimeDate, btd::TimeDate) = atd == btd
 Base.isless(atd::TimeDate, btd::TimeDate) = atd < btd
