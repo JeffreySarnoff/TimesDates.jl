e5b7d64ee239c17aa88c07a0d80172b9526210e0
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-15 04:15:59 -0400

    Update compare.jl

--- src/compare.jl
+++ src/compare.jl
@@ -1,53 +1,67 @@
-function Base.:(==)(atd::TimeDate, btd::TimeDate)
+function (==)(atd::TimeDate, btd::TimeDate)
     delta = atd - btd
     isempty(delta)
 end
-function Base.:(!=)(atd::TimeDate, btd::TimeDate)
-    delta = atd - btd
-    !isempty(delta)
-end
-function Base.:(<)(atd::TimeDate, btd::TimeDate)
-    delta = atd - btd
-    return !isempty(delta) && signbit(delta.periods[1].value)
-end
-function Base.:(<=)(atd::TimeDate, btd::TimeDate)
-    delta = atd - btd
-    return isempty(delta) || signbit(delta.periods[1].value)
-end
-function Base.:(>)(atd::TimeDate, btd::TimeDate)
-    delta = btd - atd
-    return !isempty(delta) && signbit(delta.periods[1].value)
-end
-function Base.:(>=)(atd::TimeDate, btd::TimeDate)
-    delta = btd - atd
-    return isempty(delta) || signbit(delta.periods[1].value)
-end
-Base.isequal(atd::TimeDate, btd::TimeDate) = atd == btd
-Base.isless(atd::TimeDate, btd::TimeDate) = atd < btd
 
-function Base.:(==)(atd::TimeDateZone, btd::TimeDateZone)
-    delta = atd - btd
-    isempty(delta)
-end
-function Base.:(!=)(atd::TimeDateZone, btd::TimeDateZone)
+function (!=)(atd::TimeDate, btd::TimeDate)
     delta = atd - btd
     !isempty(delta)
 end
-function Base.:(<)(atd::TimeDateZone, btd::TimeDateZone)
+
+function (<)(atd::TimeDate, btd::TimeDate)
     delta = atd - btd
     return !isempty(delta) && signbit(delta.periods[1].value)
 end
-function Base.:(<=)(atd::TimeDateZone, btd::TimeDateZone)
+
+function (<=)(atd::TimeDate, btd::TimeDate)
     delta = atd - btd
     return isempty(delta) || signbit(delta.periods[1].value)
 end
-function Base.:(>)(atd::TimeDateZone, btd::TimeDateZone)
+
+function (>)(atd::TimeDate, btd::TimeDate)
     delta = btd - atd
     return !isempty(delta) && signbit(delta.periods[1].value)
 end
-function Base.:(>=)(atd::TimeDateZone, btd::TimeDateZone)
+
+function (>=)(atd::TimeDate, btd::TimeDate)
     delta = btd - atd
     return isempty(delta) || signbit(delta.periods[1].value)
 end
-Base.isequal(atd::TimeDateZone, btd::TimeDateZone) = atd == btd
-Base.isless(atd::TimeDateZone, btd::TimeDateZone) = atd < btd
+
+isequal(atd::TimeDate, btd::TimeDate) = atd == btd
+
+isless(atd::TimeDate, btd::TimeDate) = atd < btd
+
+function (==)(atd::TimeDateZone, btd::TimeDateZone)
+    delta = atd - btd
+    isempty(delta)
+end
+
+function (!=)(atd::TimeDateZone, btd::TimeDateZone)
+    delta = atd - btd
+    !isempty(delta)
+end
+
+function (<)(atd::TimeDateZone, btd::TimeDateZone)
+    delta = atd - btd
+    return !isempty(delta) && signbit(delta.periods[1].value)
+end
+
+function (<=)(atd::TimeDateZone, btd::TimeDateZone)
+    delta = atd - btd
+    return isempty(delta) || signbit(delta.periods[1].value)
+end
+
+function (>)(atd::TimeDateZone, btd::TimeDateZone)
+    delta = btd - atd
+    return !isempty(delta) && signbit(delta.periods[1].value)
+end
+
+function (>=)(atd::TimeDateZone, btd::TimeDateZone)
+    delta = btd - atd
+    return isempty(delta) || signbit(delta.periods[1].value)
+end
+
+isequal(atd::TimeDateZone, btd::TimeDateZone) = atd == btd
+
+isless(atd::TimeDateZone, btd::TimeDateZone) = atd < btd
