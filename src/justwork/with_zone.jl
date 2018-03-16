76bb44a823c7323ba6f0d11d5985bf044c7cb6cf
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-15 20:06:00 -0400

    Update with_zone.jl

--- src/justwork/with_zone.jl
+++ src/justwork/with_zone.jl
@@ -18,23 +18,3 @@ function astimezone(x::TimeDate, tz::TimeZone)
     tdz = TimeDateZone(x)
     return astimezone(tdz, tz)
 end
-
-localtime(::Type{TimeDate}) = TimeDate(Dates.now())
-localtime(::Type{TimeDateZone}) = TimeDateZone(Dates.now(), TZ_LOCAL)
-
-uttime(::Type{TimeDate}) = TimeDate(Dates.now(Dates.UTC))
-uttime(::Type{TimeDateZone}) = TimeDateZone(Dates.now(UTC), TZ_UT)
-
-localtime(x::TimeDate) = astimezone(TimeDateZone(x), TZ_LOCAL)
-uttime(x::TimeDate) = astimezone(TimeDateZone(x), TZ_UT)
-
-localtime(x::TimeDateZone) = astimezone(x, TZ_LOCAL)
-uttime(x::TimeDateZone) = astimezone(x, TZ_UT)
-
-localtime() = localtime(TimeDateZone)
-uttime() = uttime(TimeDateZone)
-
-localtime(x::DateTime) = localtime(TimeDate(x))
-uttime(x::DateTime) = uttime(TimeDate(x))
-localtime(x::Date) = localtime(TimeDate(x))
-uttime(x::Date) = uttime(TimeDate(x))
