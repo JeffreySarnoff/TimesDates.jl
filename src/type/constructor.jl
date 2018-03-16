ea0d4ff595c5800343feec5b0de43a797a11a91f
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-16 05:27:55 -0400

    Update constructor.jl

--- src/type/constructor.jl
+++ src/type/constructor.jl
@@ -1,9 +1,90 @@
-TimeDate(dt::Date, tm::Time) = TimeDate(tm, dt)
+
 TimeDate(dtm::DateTime) = TimeDate(Time(dtm), Date(dtm))
 TimeDate(dt::Date) = TimeDate(Time(0), dt)
 TimeDate(tm::Time) = tzdefault() === tz"UTC" ?
                         TimeDate(tm, Date(now(Dates.UTC))) :
                         TimeDate(tm, Date(now()))
+
+function TimeDate(tdz::TimeDateZone)
+    at_time = tdz.at_time  # utc time
+    on_date = tdz.on_date  # utc date
+    # when the default timezone is other than UT
+    # adjusting the time, date enriches locative moment
+    if tzdefault() !== TZ_UT
+        # convert the (tm, dt) from its UT reference into the localzone
+        at_time, on_date = localtime_from_utime(at_time, on_date)
+    end
+    return TimeDate(at_time, on_date)
+end
+
+     
+TimeDate(zdt::ZonedDateTime) = TimeDate(TimeDateZone(zdt))
+
+
+
+TimeDateZone(tm::Time, dt::Date) = TimeDateZone(Time(dtm), Date(dtm), tzdefault())
+
+TimeDateZone(dtm::DateTime) = TimeDateZone(Time(dtm), Date(dtm))
+TimeDateZone(dt::Date) = TimeDateZone(Time(0), dt)
+TimeDateZone(tm::Time) = tzdefault() === tz"UTC" ?
+                        TimeDateZone(tm, Date(now(Dates.UTC))) :
+                        TimeDateZone(tm, Date(now()))
+
+function TimeDateZone(at_time::Time, on_date::Date, in_zone::Z) where {Z<:TimeZone}
+    fast_time = fasttime(at_time)
+    slow_time = at_time - fast_time
+    if in_zone !== TZ_UT
+        zdt = ZonedDateTime(on_date + slow_time, in_zone)
+        at_zone = zdt.zone
+    else
+        at_zone = in_zone
+    end
+    return TimeDateZone(at_time, on_date, in_zone, at_zone)
+end
+
+
+function TimeDateZone(tdz::TimeDate)
+    at_time = tdz.at_time  # utc time
+    fast_time = fasttime(at_time)
+    slow_time = at_time - fast_time
+    on_date = tdz.on_date  # utc date
+    in_zone = tzdefault()
+    # when the default timezone is other than UT
+    # this becomes the timezone to apply
+    if tzdefault() !== TZ_UT
+        zdt = ZonedDateTime(on_date + slow_time, in_zone)
+        at_zone = zdt.zone
+    else
+        at_zone = in_zone
+    end
+    return TimeDateZone(at_time, on_date, in_zone, at_zone)
+end
+
+     
+function TimeDateZone(zdt::ZonedDateTime)
+    datetime = zdt.utc_datetime
+    at_time = Time(datetime)
+    on_date = Date(datetime)
+    in_zone = zdt.timezone
+    at_zone = zdt.zone
+    return TimeDateZone(at_time, on_date, in_zone, at_zone)
+end
+
+
+Date(tdz::TimeDateZone) = tdz.on_date
+Time(tdz::TimeDateZone) = tdz.at_time
+DateTime(tdz::TimeDateZone) = tdz.on_date + slowtime(tdz.at_time)
+
+function ZonedDateTime(tdz::TimeDateZone)
+    datetime = tdz.on_date + slowtime(tdz.at_time)
+    return ZonedDateTime(datetime, tdz.in_zone)
+end
+
+
+
+#=
+
+
 
 function TimeDate(dtm::DateTime, moretime::P) where {P<:Union{Period, CompoundPeriod}}
    on_date = Date(dtm)
@@ -83,12 +164,5 @@ end
 TimeDateZone(dt::Date) = TimeDateZone(ZonedDateTime(dt+Time(0), tzdefault()))
 TimeDateZone(tm::Time) = TimeDateZone(TimeDate(tm), tzdefault())
 
-Date(tdz::TimeDateZone) = tdz.on_date
-Time(tdz::TimeDateZone) = tdz.at_time
-DateTime(tdz::TimeDateZone) = tdz.on_date + slowtime(tdz.at_time)
 
-function ZonedDateTime(tdz::TimeDateZone)
-    datetime = tdz.on_date + slowtime(tdz.at_time)
-    return ZonedDateTime(datetime, tdz.in_zone)
-end
-
+=#
