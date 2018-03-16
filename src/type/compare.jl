25f04047d74e45def8d80fcdb1cf4d09f73f563d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-16 00:24:00 -0400

    Update compare.jl

--- src/type/compare.jl
+++ src/type/compare.jl
@@ -2,6 +2,9 @@ function (==)(atd::TimeDate, btd::TimeDate)
     delta = atd - btd
     isempty(delta)
 end
+str2000ns    = "2000-01-01T00:00:00.123456789"
+str2000ns_ut = string(str2000ns, "+00:00")
+str2000ns_ny = string(str2000ns, "-05:00")
 
 function (!=)(atd::TimeDate, btd::TimeDate)
     delta = atd - btd
@@ -34,12 +37,12 @@ isless(atd::TimeDate, btd::TimeDate) = atd < btd
 
 function (==)(atd::TimeDateZone, btd::TimeDateZone)
     delta = atd - btd
-    isempty(delta)
+    isempty(delta) && atd.at_zone === bdt.at_zone
 end
 
 function (!=)(atd::TimeDateZone, btd::TimeDateZone)
     delta = atd - btd
-    !isempty(delta)
+    !isempty(delta) || atd.at_zone !== bdt.at_zone
 end
 
 function (<)(atd::TimeDateZone, btd::TimeDateZone)
@@ -49,18 +52,15 @@ end
 
 function (<=)(atd::TimeDateZone, btd::TimeDateZone)
     delta = atd - btd
-    return isempty(delta) || signbit(delta.periods[1].value)
+    if !isempty(delta)
+        signbit(delta.periods[1].value)
+    else
+        atd.at_zone === bdt.at_zone
+    end
 end
 
-function (>)(atd::TimeDateZone, btd::TimeDateZone)
-    delta = btd - atd
-    return !isempty(delta) && signbit(delta.periods[1].value)
-end
-
-function (>=)(atd::TimeDateZone, btd::TimeDateZone)
-    delta = btd - atd
-    return isempty(delta) || signbit(delta.periods[1].value)
-end
+(>)(atd::TimeDateZone, btd::TimeDateZone) = (<)(bdt, atd)
+(>=)(atd::TimeDateZone, btd::TimeDateZone) = (<=)(bdt, atd)
 
 isequal(atd::TimeDateZone, btd::TimeDateZone) = atd == btd
 
