46e224b95e3e0b06c2f4d8090f196f16d023e795
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-16 04:58:17 -0400

    Update nanotime.jl

--- src/type/nanotime.jl
+++ src/type/nanotime.jl
@@ -1,28 +1,46 @@
-abstract type HighResTime   <: AbstractTime end
-abstract type NanosecTime   <: HighResTime end
-abstract type HighResParts  <: HighResTime end
+abstract type NanosecondTime      <: AbstractTime   end
+abstract type NanosecondTimeBase  <: NanosecondTime end  # to define Periods, CompoundPeriods
+abstract type NanosecondBasis     <: NanosecondTime end  # a structural trait, inherited
 
-mutable struct TimeDate <: NanosecTime
+mutable struct TimeDate <: NanosecondBasis
     at_time::Time
     on_date::Date
+    
+    # ensure other constructors will be give explictly
+    
+    function TimeDate(at_time::Time, on_date::Date)
+        return new(at_time, on_date)
+    end
+    function TimeDate(on_date::Date, at_time::Time)
+        return new(at_time, on_date)
+    end
 end
 
-@inline time(x::TimeDate) = x.at_time
-@inline date(x::TimeDate) = x.on_date
+@inline attime(x::TimeDate) = x.at_time
+@inline ondate(x::TimeDate) = x.on_date
 
 TimeDate(x::TimeDate) = x
 
 
-mutable struct TimeDateZone <: NanosecTime
+mutable struct TimeDateZone{Z} <: NanosecondBasis
     at_time::Time
     on_date::Date
-    in_zone::TimeZone
+    in_zone::Z               # Z is one of {FixedTimeZone, VariableTimeZone}
     at_zone::FixedTimeZone
+
+    # ensure other constructors will be give explictly
+    
+    function TimeDateZone{Z}(at_time::Time, on_date::Date, in_zone::Z, at_zone::FixedTimeZone) where {Z<:TimeZone}
+        return new{Z}(at_time, on_date, in_zone, at_zone)
+    end
+    function TimeDateZone{Z}(on_date::Date, at_time::Time, in_zone::Z, at_zone::FixedTimeZone) where {Z<:TimeZone}
+        return new{Z}(at_time, on_date, in_zone, at_zone)
+    end
 end
 
-@inline time(x::TimeDateZone) = x.at_time
-@inline date(x::TimeDateZone) = x.on_date
-@inline timezone(x::TimeDateZone) = x.in_zone
-@inline zonetime(x::TimeDateZone) = x.at_zone
+@inline attime(x::TimeDateZone) = x.at_time
+@inline ondate(x::TimeDateZone) = x.on_date
+@inline inzone(x::TimeDateZone) = x.in_zone
+@inline atzone(x::TimeDateZone) = x.at_zone
 
 TimeDateZone(x::TimeDateZone) = x
