ba464db80b317f69967fed52fc54dd3bd5a16222
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-16 03:51:04 -0400

    Update compound.jl

--- src/type/compound.jl
+++ src/type/compound.jl
@@ -1,5 +1,78 @@
+"""
+    YearMonthDay(::Date)
 
-CompoundPeriod(dt::Date) = Year(dt) + Month(dt) + Day(dt)
+like yearmonthday() with time periods
+"""
+@inline YearMonthDay(dt::Date) =
+   reduce( +, 
+           map( (f, x)->f(x), 
+                              (Year,Month,Day),
+                              yearmonthday(dt) 
+              )
+         )
+
+"""
+    HourMinuteSecond(::Time)
+
+like yearmonthday() with time periods
+"""
+@inline HourMinuteSecond(tm::Time) =
+   reduce( +, 
+           map( (f, x)->f(x), 
+                              (Hour,Minute,Second),
+                              hourminutesecond(tm) 
+              )
+         )
+
+"""
+    hourminutesecond(::Time)
+
+like yearmonthday()
+"""
+@inline hourminutesecond(tm::Time) =
+    (x->(hour(x),minute(x),second(x))).(tm)
+
+"""
+    HourMinuteSecondMillisec(::Time)
+
+like yearmonthday() with time periods
+"""
+@inline HourMinuteSecondMillisec(tm::Time) =
+   reduce( +, 
+           map( (f, x)->f(x), 
+                    (Hour,Minute,Second,Millisecond),
+                    hourminutesecondmillisec(tm) ) )
+
+"""
+    hourminutesecondmillisec(::Time)
+
+like yearmonthday()
+"""
+@inline hourminutesecondmillisec(tm::Time) =
+    (x->(hour(x),minute(x),second(x),millisecond(x))).(tm)
+
+"""
+    MicrosecNanosec(::Time)
+
+like yearmonthday() with time periods
+"""
+@inline MicrosecNanosec(tm::Time) =
+   reduce( +, 
+           map( (f, x)->f(x), 
+                    (Microsecond, Nanosecond),
+                    microsecnanosec(tm) ) )
+
+"""
+    microsecnanosec(::Time)
+
+like yearmonthday()
+"""
+@inline microsecnanosec(tm::Time) =
+    (x->(microsec(x), nanosec(x))).(tm)
+
+
+@inline CompoundPeriod(dt::Date) = YearMonthDay(dt)
+
 CompoundPeriod(dtm::DateTime) =
    CompoundPeriod(Date(dtm)) + CompoundPeriod(Time(dtm))
 
