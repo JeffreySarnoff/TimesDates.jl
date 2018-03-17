adaf317ec7927b48be1002813304fef659a13a32
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-17 03:09:37 -0400

    Update compare.jl

--- src/type/compare.jl
+++ src/type/compare.jl
@@ -1,67 +1,98 @@
 function (==)(atd::TimeDate, btd::TimeDate)
-    delta = atd - btd
-    isempty(delta)
+    (adt.at_time === btd.at_time) &&
+    (adt.on_date === btd.on_date)
 end
-str2000ns    = "2000-01-01T00:00:00.123456789"
-str2000ns_ut = string(str2000ns, "+00:00")
-str2000ns_ny = string(str2000ns, "-05:00")
 
 function (!=)(atd::TimeDate, btd::TimeDate)
-    delta = atd - btd
-    !isempty(delta)
+    (adt.at_time !== btd.at_time) ||
+    (adt.on_date !== btd.on_date)
 end
 
 function (<)(atd::TimeDate, btd::TimeDate)
-    delta = atd - btd
-    return !isempty(delta) && signbit(delta.periods[1].value)
-end
-
-function (<=)(atd::TimeDate, btd::TimeDate)
-    delta = atd - btd
-    return isempty(delta) || signbit(delta.periods[1].value)
+    (adt.on_date < btd.on_date) ||
+    ( (adt.on_date == btd.on_date) && 
+      (adt.at_time < btd.at_time) )
 end
 
 function (>)(atd::TimeDate, btd::TimeDate)
-    delta = btd - atd
-    return !isempty(delta) && signbit(delta.periods[1].value)
+    (adt.on_date > btd.on_date) ||
+    ( (adt.on_date == btd.on_date) && 
+      (adt.at_time > btd.at_time) )
+end
+
+function (<=)(atd::TimeDate, btd::TimeDate)
+    (adt.on_date < btd.on_date) ||
+    ( (adt.on_date == btd.on_date) && 
+      (adt.at_time <= btd.at_time) )
 end
 
 function (>=)(atd::TimeDate, btd::TimeDate)
-    delta = btd - atd
-    return isempty(delta) || signbit(delta.periods[1].value)
+    (adt.on_date > btd.on_date) ||
+    ( (adt.on_date == btd.on_date) && 
+      (adt.at_time >= btd.at_time) )
 end
 
 isequal(atd::TimeDate, btd::TimeDate) = atd == btd
 
 isless(atd::TimeDate, btd::TimeDate) = atd < btd
 
+
 function (==)(atd::TimeDateZone, btd::TimeDateZone)
-    delta = atd - btd
-    isempty(delta) && atzone(atd).offset === atzone(btd).offset
-end
-
-function (!=)(atd::TimeDateZone, btd::TimeDateZone)
-    delta = atd - btd
-    !isempty(delta) || atd.at_zone.offset !== btd.at_zone.offset
-end
-
-function (<)(atd::TimeDateZone, btd::TimeDateZone)
-    delta = atd - btd
-    return !isempty(delta) && signbit(delta.periods[1].value)
-end
-
-function (<=)(atd::TimeDateZone, btd::TimeDateZone)
-    delta = atd - btd
-    if !isempty(delta)
-        signbit(delta.periods[1].value)
-    else
-        atd.at_zone.offset === btd.at_zone.offset
+    if  (atd.at_zone === btd.at_zone)
+        ( (atd.at_time === btd.at_time) &&
+          (atd.on_date === btd.on_date)   )
+    else 
+        astimezone(atd, TZ_UT) == astimezone(btd, TZ_UT)
     end
 end
 
-(>)(atd::TimeDateZone, btd::TimeDateZone) = (<)(btd, atd)
-(>=)(atd::TimeDateZone, btd::TimeDateZone) = (<=)(btd, atd)
+function (!=)(atd::TimeDateZone, btd::TimeDateZone)
+    if  (atd.at_zone === btd.at_zone)
+        ( (atd.at_time !== btd.at_time) ||
+          (atd.on_date !== btd.on_date)   )
+    else 
+        astimezone(atd, TZ_UT) != astimezone(btd, TZ_UT)
+    end
+end
 
-isequal(atd::TimeDateZone, btd::TimeDateZone) = atd == btd
+function (<)(atd::TimeDateZone, btd::TimeDateZone)
+    if  (atd.at_zone === btd.at_zone)
+        ( (atd.on_date < btd.on_date) ||
+          (  (atd.on_date === btd.on_date) &&
+             (atd.at_time < btd.at_time)      ) )
+    else 
+        astimezone(atd, TZ_UT) < astimezone(btd, TZ_UT)
+    end
+end
 
-isless(atd::TimeDateZone, btd::TimeDateZone) = atd < btd
+function (>)(atd::TimeDateZone, btd::TimeDateZone)
+    if  (atd.at_zone === btd.at_zone)
+        ( (atd.on_date > btd.on_date) ||
+          (  (atd.on_date === btd.on_date) &&
+             (atd.at_time > btd.at_time)      ) )
+    else 
+        astimezone(atd, TZ_UT) > astimezone(btd, TZ_UT)
+    end
+end
+
+
+function (<=)(atd::TimeDateZone, btd::TimeDateZone)
+    if  (atd.at_zone === btd.at_zone)
+        ( (atd.on_date < btd.on_date) ||
+          (  (atd.on_date === btd.on_date) &&
+             (atd.at_time <= btd.at_time)      ) )
+    else 
+        astimezone(atd, TZ_UT) <= astimezone(btd, TZ_UT)
+    end
+end
+
+function (>=)(atd::TimeDateZone, btd::TimeDateZone)
+    if  (atd.at_zone === btd.at_zone)
+        ( (atd.on_date > btd.on_date) ||
+          (  (atd.on_date === btd.on_date) &&
+             (atd.at_time >= btd.at_time)      ) )
+    else 
+        astimezone(atd, TZ_UT) => astimezone(btd, TZ_UT)
+    end
+end
+
