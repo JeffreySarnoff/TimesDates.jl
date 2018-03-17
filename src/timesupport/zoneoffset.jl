82df9f4e59f75ee8d6b09cd93c78455963fcb775
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-16 22:40:59 -0400

    Update zoneoffset.jl

--- src/timesupport/zoneoffset.jl
+++ src/timesupport/zoneoffset.jl
@@ -2,11 +2,18 @@
     this logic is developed relative to universal time 
 =#
 
-offset_seconds_from_ut(offset::UTCOffset) = offset.std.value + offset.dst.value
+offset_seconds_from_ut(offset::UTCOffset) = offset.std.value + offset.dst.valuea
 offset_Seconds_from_ut(offset::UTCOffset) = Second(offset_seconds_from_ut(offset))
 
+function offset_minutes_seconds_from_ut(offset::UTC0ffset)
+    secs = offset_seconds_from_ut(offset)
+    mins, secs = fldmod(secs, SECONDS_PER_MINUTE)
+    
 offset_minutes_from_ut(offset::UTCOffset) = div(offset_seconds_from_ut, MINUTES_PER_HOUR)
 offset_hours_from_ut(offset::UTCOffset)   = div(offset_hours_from_ut, HOURS_PER_DAY)
+
+function minutes_to_hours_minutes(x::Minutes)
+
 function offset_qrtrhours_from_ut(offset::UTCOffset)
     secs = offset_seconds_from_ut(offset)
     mins, secs = fldmod(secs)
