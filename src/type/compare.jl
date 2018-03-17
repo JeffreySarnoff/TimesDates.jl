6de2e5fce20ce5282e95398b55168e705a2efdc0
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-17 10:08:42 -0400

    Update compare.jl

--- src/type/compare.jl
+++ src/type/compare.jl
@@ -1,35 +1,35 @@
 function (==)(atd::TimeDate, btd::TimeDate)
-    (adt.at_time === btd.at_time) &&
-    (adt.on_date === btd.on_date)
+    (atd.at_time === btd.at_time) &&
+    (atd.on_date === btd.on_date)
 end
 
 function (!=)(atd::TimeDate, btd::TimeDate)
-    (adt.at_time !== btd.at_time) ||
-    (adt.on_date !== btd.on_date)
+    (atd.at_time !== btd.at_time) ||
+    (atd.on_date !== btd.on_date)
 end
 
 function (<)(atd::TimeDate, btd::TimeDate)
-    (adt.on_date < btd.on_date) ||
-    ( (adt.on_date == btd.on_date) && 
-      (adt.at_time < btd.at_time) )
+    (atd.on_date < btd.on_date) ||
+    ( (atd.on_date == btd.on_date) && 
+      (atd.at_time < btd.at_time) )
 end
 
 function (>)(atd::TimeDate, btd::TimeDate)
-    (adt.on_date > btd.on_date) ||
-    ( (adt.on_date == btd.on_date) && 
-      (adt.at_time > btd.at_time) )
+    (atd.on_date > btd.on_date) ||
+    ( (atd.on_date == btd.on_date) && 
+      (atd.at_time > btd.at_time) )
 end
 
 function (<=)(atd::TimeDate, btd::TimeDate)
-    (adt.on_date < btd.on_date) ||
-    ( (adt.on_date == btd.on_date) && 
-      (adt.at_time <= btd.at_time) )
+    (atd.on_date < btd.on_date) ||
+    ( (atd.on_date == btd.on_date) && 
+      (atd.at_time <= btd.at_time) )
 end
 
 function (>=)(atd::TimeDate, btd::TimeDate)
-    (adt.on_date > btd.on_date) ||
-    ( (adt.on_date == btd.on_date) && 
-      (adt.at_time >= btd.at_time) )
+    (atd.on_date > btd.on_date) ||
+    ( (atd.on_date == btd.on_date) && 
+      (atd.at_time >= btd.at_time) )
 end
 
 isequal(atd::TimeDate, btd::TimeDate) = atd == btd
