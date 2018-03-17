5afcc85368113b23d2457e3e6d2de7d75913b634
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-17 11:36:50 -0400

    Update convert_into_others.jl

--- src/type/convert_into_others.jl
+++ src/type/convert_into_others.jl
@@ -1,11 +1,11 @@
 
-Date(td::TimeDate) = td.on_date
+#Date(td::TimeDate) = td.on_date
 
-Time(td::TimeDate) = td.at_time
+#Time(td::TimeDate) = td.at_time
 
-DateTime(td::TimeDate) = td.on_date + slowtime(td.at_time)
+#DateTime(td::TimeDate) = td.on_date + slowtime(td.at_time)
 
-DateTime(tm::Time) = Date(now()) + slowtime(tm)
+#DateTime(tm::Time) = Date(now()) + slowtime(tm)
 
 
 Date(tdz::TimeDateZone) = tdz.on_date
@@ -17,8 +17,8 @@ DateTime(tdz::TimeDateZone) = tdz.on_date + slowtime(tdz.at_time)
 ZonedDateTime(tdz::TimeDateZone) = ZonedDateTime(DateTime(tdz), tdz.timezone)
 
 
-function ZonedDateTime(tdz::TimeDateZone)
-    datetime = tdz.on_date + slowtime(tdz.at_time)
-    return ZonedDateTime(datetime, tdz.in_zone)
-end
+#function ZonedDateTime(tdz::TimeDateZone)
+#    datetime = tdz.on_date + slowtime(tdz.at_time)
+#    return ZonedDateTime(datetime, tdz.in_zone)
+#end
 
