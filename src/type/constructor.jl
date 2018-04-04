b98232f4d8864519723ada0a816590c3b7c4e09a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-04-04 09:12:40 -0400

    Update constructor.jl

--- src/type/constructor.jl
+++ src/type/constructor.jl
@@ -3,6 +3,29 @@ TimeDate(dtm::DateTime) = TimeDate(Time(dtm), Date(dtm))
 TimeDate(dt::Date) = TimeDate(Time(0), dt)
 TimeDate(tm::Time) = throw(ErrorException("TimeDate(::Time) is not used"))
 
+function TimeDate(dtm::DateTime, period::P) where {P<:Period}
+    timedate = TimeDate(dtm)
+    timedate += period
+    return timedate
+end
+
+function TimeDate(dtm::DateTime, cperiod::CompoundPeriod)
+    timedate = TimeDate(dtm)
+    timedate = timedate + cperiod
+    return timedate
+end
+
+@inline function TimeDate(td::TimeDate, period::P) where {P<:Period}
+    td += period
+    return td
+end
+
+@inline function TimeDate(td::TimeDate, cperiod::CompoundPeriod)
+    td += cperiod
+    return td
+end
+
+
 function TimeDate(tdz::TimeDateZone)
     timedate = TimeDate(tdz.at_time, tdz.on_date)
     offset = tdz.at_zone.offset.std + tdz.at_zone.offset.dst
