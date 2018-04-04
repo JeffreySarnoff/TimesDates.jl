2f9d7d7e23abed13e0b066562e56e28ba83fb845
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-04-04 06:51:07 -0400

    Update constructor.jl

--- src/type/constructor.jl
+++ src/type/constructor.jl
@@ -3,6 +3,13 @@ TimeDate(dtm::DateTime) = TimeDate(Time(dtm), Date(dtm))
 TimeDate(dt::Date) = TimeDate(Time(0), dt)
 TimeDate(tm::Time) = throw(ErrorException("TimeDate(::Time) is not used"))
 
+function TimeDate(tdz::TimeDateZone)
+    timedate = TimeDate(tdz.at_time, tdz.on_date)
+    offset = tdz.at_zone.offset.std + tdz.at_zone.offset.dst
+    timedate += offset
+    return timedate
+end
+
 TimeDateZone(zdt::ZonedDateTime) =
     TimeDateZone(Time(zdt.utc_datetime), Date(zdt.utc_datetime), zdt.timezone, zdt.zone)
 TimeDateZone(dtm::DateTime) = TimeDateZone(dtm, tzdefault())
@@ -13,6 +20,7 @@ TimeDateZone(tm::Time) = throw(ErrorException("TimeDateZone(::Time) is not used"
 DateTime(td::TimeDate) = DateTime(td.on_date + slowtime(td.at_time))
 Date(td::TimeDate) = td.on_date
 Time(td::TimeDate) = td.at_time
+DateTime(tdz::TimeDateZone) = DateTime(TimeDate(tdz))
 
 function ZonedDateTime(td::TimeDate, tz::TimeZone)
     datetime = td.on_date + slowtime(td.at_time)
@@ -20,7 +28,9 @@ function ZonedDateTime(td::TimeDate, tz::TimeZone)
     return zdt
 end
 ZonedDateTime(tm::Time, dt::Date, tz::TimeZone) =
-    ZonedDateTime(dt+tm, tz)
+    ZonedDateTime(TimeDate(tm,dt), tz)
+
+ZonedDateTime(tdz::TimeDateZone) = ZonedDateTime(TimeDate(tdz), tdz.in_zone)
 
 function TimeDateZone(td::TimeDate, tz::FixedTimeZone)
     TimeDateZone(td.at_time, td.on_date, tz, tz)
@@ -39,4 +49,7 @@ function TimeDateZone(tm::Time, dt::Date, tz::VariableTimeZone)
                        zdt.timezone, zdt.zone)
     return tdz
 end
+
+TimeDateZone(dt::DateTime, tz::VariableTimeZone) = TimeDateZone(TimeDate(dt), tz)
+
 
