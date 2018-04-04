a796f5191b315d0163b778d5be672917d350dc0e
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-04-04 06:36:34 -0400

    Update utime_localtime.jl

--- src/timesupport/utime_localtime.jl
+++ src/timesupport/utime_localtime.jl
@@ -1,10 +1,10 @@
 
 localtime(::Type{TimeDate}) = TimeDate(now())
-localtime(::Type{TimeDateZone}) = TimeDateZone(now(), TZ_LOCAL)
+localtime(::Type{TimeDateZone}) = TimeDateZone(TimeDate(now()), TZ_LOCAL)
 localtime() = localtime(TimeDateZone)
 
 utime(::Type{TimeDate}) = TimeDate(now(Dates.UTC))
-utime(::Type{TimeDateZone}) = TimeDateZone(now(Dates.UTC), TZ_UT)
+utime(::Type{TimeDateZone}) = TimeDateZone(TimeDate(now(Dates.UTC)), TZ_UT)
 utime() = utime(TimeDateZone)
 
 localtime(x::TimeDate) = astimezone(TimeDateZone(x), TZ_LOCAL)
@@ -13,8 +13,6 @@ utime(x::TimeDate) = astimezone(TimeDateZone(x), TZ_UT)
 localtime(x::TimeDateZone) = astimezone(x, TZ_LOCAL)
 utime(x::TimeDateZone) = astimezone(x, TZ_UT)
 
-localtime() = localtime(TimeDateZone)
-utime() = utime(TimeDateZone)
 
 localtime(x::DateTime) = localtime(TimeDate(x))
 utime(x::DateTime) = utime(TimeDate(x))
