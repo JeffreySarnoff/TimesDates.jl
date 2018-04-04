438a83c2e132b842f3912db57df166b7b2e2a433
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-04-04 07:05:34 -0400

    Update astimezone.jl

--- src/timezone/astimezone.jl
+++ src/timezone/astimezone.jl
@@ -4,24 +4,23 @@ function astimezone(x::TimeDateZone, tz::FixedTimeZone)
     on_date = x.on_date
     at_time = x.at_time
     fast_time = fasttime(at_time)
-   
-    zdt = ZonedDateTime(x)
-    zdt_offset = value(zdt.zone.offset)
-    tz_offset = value(tz.offset)
     
+    zdt = ZonedDateTime(x)
     zdt = astimezone(zdt, tz)
     tdz = TimeDateZone(zdt)
-    
-    if zdt_offset <= tz_offset
-        tdz = tdz + fast_time
-    else
-        tdz = tdz - fast_time
-    end
-    
+    tdz = tdz + fast_time
     return tdz
 end
 
-function astimezone(x::TimeDate, tz::FixedTimeZone)
-    tdz = TimeDateZone(x.at_time, x.on_date, tz, tz)
+function astimezone(x::TimeDateZone, tz::VariableTimeZone)
+    on_date = x.on_date
+    at_time = x.at_time
+    fast_time = fasttime(at_time)
+    
+    zdt = ZonedDateTime(x)
+    zdt = astimezone(zdt, tz)
+    tdz = TimeDateZone(zdt)
+    tdz = tdz + fast_time
     return tdz
 end
+
