c117da480ba6ce8ba8c0e14a9392720e13dfb12f
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-04-05 02:51:29 -0400

    Update timezones.jl

--- src/timezone/timezones.jl
+++ src/timezone/timezones.jl
@@ -6,3 +6,14 @@ TimeZone(tz::FixedTimeZone) = tz
 TimeZone(tz::VariableTimeZone) = tz
 TimeZone(zdt::ZonedDateTime) = zdt.timezone
 TimeZone(tdz::TimeDateZone) = tdz.in_zone
+
+
+function astimezone(x::TimeDateZone, tz::T) where
+                    {T<:Union{FixedTimeZone,VariableTimeZone}}
+    fast_time = fasttime(x)
+    zdt = ZonedDateTime(x)
+    zdt = astimezone(zdt, tz)
+    tdz = TimeDateZone(zdt)
+    tdz = tdz + fast_time
+    return tdz
+end
