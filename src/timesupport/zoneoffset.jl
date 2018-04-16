79e95464a415517aa4ac8e3644211b83499a9572
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-04-15 23:11:03 -0400

    Update zoneoffset.jl

--- src/timesupport/zoneoffset.jl
+++ src/timesupport/zoneoffset.jl
@@ -2,12 +2,20 @@
     this logic is developed relative to universal time
 =#
 
-offset_from_ut(tz::FixedTimeZone) = offset_seconds_from_ut(tz.offset)
-offset_from_ut(zdt::ZonedDateTime) = offset_seconds_from_ut(zdt.zone.offset)
-offset_from_ut(tdz::TimeDateZone) = offset_seconds_from_ut(at_zone(tdz))
+@inline offset_from_ut(tz::FixedTimeZone) =
+    offset_seconds_from_ut(tz.offset)
+@inline offset_from_ut(dtm::DateTime, tz::VariableTimeZone) =
+    offset_seconds_from_ut(ZonedDateTime(dtm, tz))
 
-offset_seconds_from_ut(offset::UTCOffset) = offset.std.value + offset.dst.value
-offset_Seconds_from_ut(offset::UTCOffset) = Second(offset_seconds_from_ut(offset))
+@inline offset_from_ut(zdt::ZonedDateTime) =
+    offset_seconds_from_ut(zdt.zone.offset)
+@inline offset_from_ut(tdz::TimeDateZone) =
+    offset_seconds_from_ut(ZonedDateTime(slowtime(tdz), in_zone(tdz)))
+
+@inline offset_seconds_from_ut(offset::UTCOffset) =
+    offset.std.value + offset.dst.value
+@inline offset_Seconds_from_ut(offset::UTCOffset) =
+    Second(offset_seconds_from_ut(offset))
 
 function offset_minutes_seconds_from_ut(offset::UTCOffset)
     secs = offset_seconds_from_ut(offset)
@@ -21,7 +29,8 @@ function offset_minutes_from_ut(offset::UTCOffset)
     return mins
 end
 
-offset_Minutes_from_ut(offset::UTCOffset) = Minutes(offset_minutes_from_ut(offset))
+@inline offset_Minutes_from_ut(offset::UTCOffset) =
+    Minutes(offset_minutes_from_ut(offset))
 
 function offset_minutes_hours_from_ut(offset::UTCOffset)
     mins = offset_minutes_from_ut(offset)
@@ -38,9 +47,10 @@ function offset_qrtrhours_from_ut(offset::UTCOffset)
     return Minute(mins)
 end
 
-offset_hours_from_ut(offset::UTCOffset)   = div(offset_hours_from_ut, HOURS_PER_DAY)
+offset_hours_from_ut(offset::UTCOffset) =
+    div(offset_hours_from_ut, HOURS_PER_DAY)
 
-function minutes_to_hours_minutes(x::Minute)
+@inline function minutes_to_hours_minutes(x::Minute)
     hours, mins = fldmod(x.value, MINUTES_PER_HOUR)
     return Hour(hours) + Minute(mins)
 end
