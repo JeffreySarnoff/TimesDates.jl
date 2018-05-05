5769a195f894f0e191d10f1a28d819b031053a26
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-05-05 01:30:20 -0400

    Update TimesDates.jl

--- src/TimesDates.jl
+++ src/TimesDates.jl
@@ -37,18 +37,18 @@ import TimeZones: @tz_str, ZonedDateTime, TimeZone,
 
 import TimeZones.value # value(offset::UTCOffset) = value(offset.std + offset.dst)
 
-const AkoTimeZone = Union{FixedTimeZone, VariableTimeZone}
 
+include("TimeDate.jl")
+include("TimeDateZone.jl")
+include("timeconsts.jl")
+
+const AkoTimeZone = Union{FixedTimeZone, VariableTimeZone}
 
 Date(x::Date) = x
 Time(x::Time) = x
 DateTime(x::DateTime) = x
 ZonedDateTime(x::ZonedDateTime) = x
 
-
-include("TimeDate.jl")
-include("TimeDateZone.jl")
-include("timeconsts.jl")
 include("periods.jl")
 include("select.jl")
 include("arith.jl")
