9a6ee93aebf1b50ac7510da621b1d8c62da258c9
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-05-09 14:08:06 -0400

    Update passthru.jl

--- src/passthru.jl
+++ src/passthru.jl
@@ -1,7 +1,9 @@
 for fn in (:yearmonthday, :yearmonth, :monthday, :dayofmonth,
            :dayofweek, :isleapyear, :daysinmonth, :daysinyear, :dayofyear, :dayname, :dayabbr,
            :dayofweekofmonth, :daysofweekinmonth, :monthname, :monthabbr,
-           :quarterofyear, :dayofquarter)
+           :quarterofyear, :dayofquarter,
+           :firstdayofweek, :lastdayofweek, :firstdayofmonth, :lastdayofmonth,
+           :firstdayofyear, :lastdayofyear, :firstdayofquarter, :lastdayofquarter)
     @eval $fn(td::TimeDate) = $fn(Date(td))
     @eval $fn(tdz::TimeDateZone) = $fn(Date(td))
 end
