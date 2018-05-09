7bb80681a28965cc174c0db3fa8ae58366b8ddce
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-05-09 15:47:30 -0400

    Update passthru.jl

--- src/passthru.jl
+++ src/passthru.jl
@@ -1,9 +1,22 @@
 for fn in (:yearmonthday, :yearmonth, :monthday, :dayofmonth,
            :dayofweek, :isleapyear, :daysinmonth, :daysinyear, :dayofyear, :dayname, :dayabbr,
            :dayofweekofmonth, :daysofweekinmonth, :monthname, :monthabbr,
-           :quarterofyear, :dayofquarter,
-           :firstdayofweek, :lastdayofweek, :firstdayofmonth, :lastdayofmonth,
-           :firstdayofyear, :lastdayofyear, :firstdayofquarter, :lastdayofquarter)
+           :quarterofyear, :dayofquarter)
     @eval $fn(td::TimeDate) = $fn(Date(td))
     @eval $fn(tdz::TimeDateZone) = $fn(Date(tdz))
 end
+
+for fn in (:firstdayofweek, :lastdayofweek, :firstdayofmonth, :lastdayofmonth,
+           :firstdayofyear, :lastdayofyear, :firstdayofquarter, :lastdayofquarter)
+    @eval $fn(td::TimeDate) = $fn(Date(td))
+    @eval $fn(tdz::TimeDateZone) = TimeDateZone($fn(Date(tdz)),in_zone(tdz))
+end
+
+for P in (:Hour, :Minute, :Second, :Millisecond, :Microsecond, :Nanosecond)
+   @eval begin
+       (+)(dt::Date, period::$P) = TimeDate(dt) + period
+       (-)(dt::Date, period::$P) = TimeDate(dt) - period
+   end
+end
+
+           
