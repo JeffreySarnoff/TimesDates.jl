9f671ae89f765340c8cf7a31811b6893cea5d99d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-07-30 18:30:15 -0400

    Update compare.jl

--- src/compare.jl
+++ src/compare.jl
@@ -25,9 +25,43 @@ function (>=)(x::TimeDate, y::TimeDate)
     (dtx > dty) || ((dtx === dty) && (Time(x) >= Time(y)))
 end
 
+function isequal(x::TimeDate, y::TimeDate)
+    xx = Microsecond(x) + Nanosecond(x)
+    yy = Microsecond(y) + Nanosecond(y)
+    zx = DateTime(x)
+    zy = DateTime(y)
+    isequal(zx, zy) && isequal(xx, yy)
+end
+
+function isless(x::TimeDate, y::TimeDate)
+    xx = Microsecond(x) + Nanosecond(x)
+    yy = Microsecond(y) + Nanosecond(y)
+    zx = DateTime(x)
+    zy = DateTime(y)
+    isless(zx, zy) || (isequal(zx,zy) && isless(xx, yy))
+end
+
+isequal(x::TimeDate, y::DateTime) = isequal(x, TimeDate(y))
+isequal(x::DateTime, y::TimeDate) = isequal(TimeDate(x), y)
 
 isless(x::TimeDate, y::DateTime) = isless(x, TimeDate(y))
 isless(x::DateTime, y::TimeDate) = isless(TimeDate(x), y)
+
+
+ (<)(x::TimeDate, y::DateTime) =  (<)(x, TimeDate(y))
+ (>)(x::TimeDate, y::DateTime) =  (>)(x, TimeDate(y))
+(<=)(x::TimeDate, y::DateTime) = (<=)(x, TimeDate(y))
+(>=)(x::TimeDate, y::DateTime) = (>=)(x, TimeDate(y))
+(==)(x::TimeDate, y::DateTime) = (==)(x, TimeDate(y))
+(!=)(x::TimeDate, y::DateTime) = (!=)(x, TimeDate(y))
+
+ (<)(x::DateTime, y::TimeDate) =  (<)(TimeDate(x), y)
+ (>)(x::DateTime, y::TimeDate) =  (>)(TimeDate(x), y)
+(<=)(x::DateTime, y::TimeDate) = (<=)(TimeDate(x), y)
+(>=)(x::DateTime, y::TimeDate) = (>=)(TimeDate(x), y)
+(==)(x::DateTime, y::TimeDate) = (==)(TimeDate(x), y)
+(!=)(x::DateTime, y::TimeDate) = (!=)(TimeDate(x), y)
+
 
 function isequal(x::TimeDateZone, y::TimeDateZone)
     xx = Microsecond(x) + Nanosecond(x)
@@ -45,9 +79,31 @@ function isless(x::TimeDateZone, y::TimeDateZone)
     isless(zx, zy) || (isequal(zx,zy) && isless(xx, yy))
 end
 
-(<)(x::TimeDateZone, y::TimeDateZone) = isless(x,y)
-(>)(x::TimeDateZone, y::TimeDateZone) = isless(y,x)
+
+ (<)(x::TimeDateZone, y::TimeDateZone) = isless(x,y)
+ (>)(x::TimeDateZone, y::TimeDateZone) = isless(y,x)
 (<=)(x::TimeDateZone, y::TimeDateZone) = isless(x,y) || isequal(x,y)
 (>=)(x::TimeDateZone, y::TimeDateZone) = isless(y,x) || isequal(x,y)
 (==)(x::TimeDateZone, y::TimeDateZone) = isequal(x,y)
 (!=)(x::TimeDateZone, y::TimeDateZone) = !isequal(x,y)
+
+
+isequal(x::TimeDateZone, y::ZonedDateTime) = isequal(x, TimeDateZone(y))
+isequal(x::ZonedDateTime, y::TimeDateZone) = isequal(TimeDateZone(x), y)
+
+isless(x::TimeDateZone, y::ZonedDateTime) = isless(x, TimeDateZone(y))
+isless(x::ZonedDateTime, y::TimeDateZone) = isless(TimeDateZone(x), y)
+
+ (<)(x::TimeDateZone, y::ZonedDateTime) =  (<)(x, TimeDateZone(y))
+ (>)(x::TimeDateZone, y::ZonedDateTime) =  (>)(x, TimeDateZone(y))
+(<=)(x::TimeDateZone, y::ZonedDateTime) = (<=)(x, TimeDateZone(y))
+(>=)(x::TimeDateZone, y::ZonedDateTime) = (>=)(x, TimeDateZone(y))
+(==)(x::TimeDateZone, y::ZonedDateTime) = (==)(x, TimeDateZone(y))
+(!=)(x::TimeDateZone, y::ZonedDateTime) = (!=)(x, TimeDateZone(y))
+
+ (<)(x::ZonedDateTime, y::TimeDateZone) =  (<)(TimeDateZone(x), y)
+ (>)(x::ZonedDateTime, y::TimeDateZone) =  (>)(TimeDateZone(x), y)
+(<=)(x::ZonedDateTime, y::TimeDateZone) = (<=)(TimeDateZone(x), y)
+(>=)(x::ZonedDateTime, y::TimeDateZone) = (>=)(TimeDateZone(x), y)
+(==)(x::ZonedDateTime, y::TimeDateZone) = (==)(TimeDateZone(x), y)
+(!=)(x::ZonedDateTime, y::TimeDateZone) = (!=)(TimeDateZone(x), y)
