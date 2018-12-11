ccfce561473e404b602dbd0bb7187bde508e982d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-12-11 12:49:05 -0500

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,1 +1,16 @@
+using Documenter, TimesDates, CompoundPeriods, Dates
 
+makedocs(
+    modules = [TimesDates],
+    sitename = "TimesDates",
+    pages  = Any[
+        "Overview"                 => "index.md",
+        "References"               => "references.md",
+        "Index"                    => "documentindex.md"
+        ]
+    )
+
+deploydocs(
+    repo = "github.com/JeffreySarnoff/TimesDates.jl.git",
+    target = "build"
+)
